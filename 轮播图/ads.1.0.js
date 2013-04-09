dca5ae7e4b420aefbf69e8beb27206e0dd614c20
shishaokun <741653990@qq.com>
2013-04-09 11:21:43 +0800

    lunbotu

---
+++ 轮播图/ads.1.0.js
@@ -0,0 +1,159 @@
+﻿/*
+ *author: eric
+ *
+ *deriction: 图像轮播插件
+ *
+ *title: carousel
+ *
+*/
+
+function Carousel_factory(){};
+Carousel_factory.prototype = {
+	obj: null,//对象
+	Keep: null,//图片等待移动时间(ms)
+	Speed: null,//图片移动间隔时间(ms)
+	Space: null,//图片移动距离(px)
+	LockMove: false,//移动是否被锁住
+	ObjOver: 0,//对象移过的距离(px)
+	AutoMoveObj: null,//自动移动对象
+	_flag: 'l',//移动方向(r/l)
+	
+	ResetImg: function(){//重设图片大小
+		var obj = document.getElementById(this.obj);
+		var objh = obj.offsetHeight;
+		var objw = obj.offsetWidth;
+		var objlist = obj.getElementsByTagName('img');
+		for(var i = 0,objlistlen = objlist.length; i < objlistlen; i++){
+			var imgh = objlist[i].scrollHeight;
+			var imgw = objlist[i].scrollWidth;
+			if(objh/objw < imgh/imgw){
+				objlist[i].className = 'reset_h';
+			}else{
+				objlist[i].className = 'reset_w';
+			}
+		}
+	},
+	
+	AutoMove: function(){//自动移动
+		console.log('AutoMove');
+		clearInterval(this.AutoMoveObj);
+		this.AutoMoveObj = setInterval('this.GoDown',this.Keep);
+	},
+	
+	GoDown: function(){//向左移动
+		console.log('Down');
+		if(!this.LockMove){
+			clearInterval(this.AutoMoveObj);
+			this.LockMove = true;
+			this._flag = 'l'
+			this.MoveFlag();
+		}
+		this.MoveOrNot();
+	},
+	
+	GoUp: function(){//向右移动
+		consle.log('Go');
+		this._flag = 'r'
+		if(!MoveLock){
+			claerInterval(this.AutoMoveObj);
+			this.LockMove = true;
+			this.MoveFlag();
+		}
+		this.MoveOrNot();
+	},
+	
+	MoveFlag: function(){//移动对象一次作为移动锁住标志
+		var objlistwid = document.getElementById(this.objlist).offsetWidth;
+		if(this._flag == 'r'){
+			console.log('MoveRight');
+			if(this.objViewScroLeft <= 0){
+				console.log('changeright');
+				this.objViewScroLeft += objlistwid;
+			}
+			this.objViewScroLeft -= this.Space;
+		}else{
+			console.log('MoveLeft');
+			if(this.objViewScroLeft >= objlistwid){
+				console.log('changeleft');
+				this.objViewScroLeft -= objlistwid;
+			}
+			this.objViewScroLeft += this.Space;
+		}
+	},
+	
+	MoveOrNot: function(){//是否继续移动
+		if(this.objViewScroLeft % this.ObjWid != 0){
+			this.ObjOver = this.objViewScroLeft % this.ObjWid;
+			this.ObjOver = (this._flag == 'r') ? (-this.ObjOver) : (this.ObjWid - this.ObjOver);
+			this.MoveAction();
+		}else {
+			this.LockMove = false;
+		}
+		this.AutoMove();
+	},
+	
+	MoveAction: function(){//
+		var Movedis;//移动距离
+		if(this.ObjWid == 0){
+			this.lockMove = false;
+			return;
+			}
+		if(this.ObjWid < 0){ //右移动
+			if(this.ObjWid < -this.Space){
+				this.ObjWid += this.Space;
+				Movedis = this.Space;
+			}else{
+				Movedis = -this.ObjWid;
+				this.ObjWid = 0;
+			}
+			this.objViewScrollLeft -= Movedis;
+			setTimeout('this.moveAction()',this.speed);
+		}else{//左移动
+			if(this.ObjWid > this.Space){
+				this.ObjWid -= this.Space;
+				Movedis = this.Space;
+			}else{
+				Movedis = this.ObjWid;
+				this.ObjWid = 0;
+			}
+			this.objViewScrollLeft += Movedis;
+			setTimeout('this.MoveAction()',this.Speed);
+		}
+	},
+	
+	
+	ObjWid: function(){//获取移动对象的宽度
+		var objwidth = document.getElementById(this.objview).offsetWidth;
+		return objwidth;
+	},
+	
+	objViewScroLeft: function (){//获取对象移动宽度
+		var objViewScrollLeft = document.getElementById(this.objview).scrollLeft;
+		return objViewScrollLeft;
+	},
+	
+	objview: function(){//获取对象视窗
+		var _ObjView = this.obj + 'view';
+		return _ObjView;
+	},
+	
+	objlist: function(){//获取对象list
+		var _ObjList = this.obj + 'list';
+		return _ObjList;
+	},
+	
+};
+
+
+	
+
+function carousel_img(obj){
+	var carousel = new Carousel_factory();
+	carousel.obj = obj;//传入对象
+	carousel.Keep = 3000;//间隔时间
+	carousel.Speed = 10;//每次移动的间隔时间
+	carousel.Space = 5;//每次移动的距离
+	carousel.AutoMove();//开始运行自动移动
+}
+
+carousel_img('ads');
---
+++ 轮播图/ads.js
@@ -0,0 +1,133 @@
+﻿document.getElementById('adslistcopy').innerHTML = document.getElementById('adslist').innerHTML;
+
+//设置图片大小
+var adsbox = document.getElementById('adsview');
+var adsboxh = adsbox.offsetHeight;
+var adsboxw = adsbox.offsetWidth;
+//console.log(adsboxh + " " + adsboxw);
+var adslist = adsbox.getElementsByTagName('img');
+for(var i = 0,adslistlen = adslist.length; i < adslistlen; i++){
+	var imgh = adslist[i].scrollHeight;
+	var imgw = adslist[i].scrollWidth;
+	if(adsboxh/adsboxw < imgh/imgw){
+		adslist[i].className = 'imgheight';
+	}else{
+		adslist[i].className = 'imgwidth';
+	}
+	//console.log(imgh + " " + imgw);
+}
+
+
+var keep = 3000;//间隔时间
+var speed = 10;//每次移动的间隔时间
+var Space = 5;//每次移动的距离
+var lockMove = false;//移动是否被锁住
+var com = 0;//移动过的距离
+var adsview = document.getElementById('adsview');//获取移动对象
+var firstlistwid = document.getElementById('adslist').offsetWidth;//获取第一个list的宽度
+var size = adsview.offsetWidth;//box宽度
+var autoMoveObj;//自动移动obj
+
+Automove();
+
+//自动移动
+function Automove (){
+	console.log('automove');
+	clearInterval(autoMoveObj);
+	autoMoveObj = setInterval('GoDown()',keep);
+}
+
+//向右移动
+function GoUp(){
+	console.log('up');
+	if(!lockMove){
+		clearInterval(autoMoveObj);
+		lockMove = true;
+		MoveFlag('r');
+	}
+	moveOrNot('r');
+}
+
+//向左移动
+function GoDown(){
+	console.log('Down');
+	if(!lockMove){
+		clearInterval(autoMoveObj);
+		lockMove = true;
+		MoveFlag('l');
+	}
+	moveOrNot('l');
+}
+
+//移动一点作为移动锁住的标志
+function MoveFlag(flag){
+	if(flag == 'r'){
+		console.log('moveFlagRight');
+		if(adsview.scrollLeft <= 0){
+			adsview.scrollLeft = adsview.scrollLeft + firstlistwid;
+		}
+		adsview.scrollLeft -= Space;
+		console.log('MoveRight');
+	}else if(flag == 'l'){
+		console.log('moveFlagLeft' + " " + adsview.scrollLeft + " " + firstlistwid);
+		if(adsview.scrollLeft >= firstlistwid){
+			console.log('changeleft');
+			adsview.scrollLeft = adsview.scrollLeft - firstlistwid;
+		}
+		adsview.scrollLeft += Space;
+		console.log('MoveLeft');
+	}
+}
+
+//继续移动否
+function moveOrNot(flag){
+	if(adsview.scrollLeft%size != 0){
+		com = adsview.offsetLeft%size;
+		com = (flag == 'r') ? (-com) : (size - com);
+		moveAction();
+	}else{
+		lockMove = false;
+	}
+	Automove();
+}
+
+//移动
+function moveAction(){
+	var comdis;//移动距离
+	if(com == 0){
+		lockMove = false;
+		return;
+	}
+	if(com < 0){ //右移动
+		if(com < -Space){
+			com += Space;
+			comdis = Space;
+		}else{
+			comdis = -com;
+			com = 0;
+		}
+		adsview.scrollLeft -= comdis;
+		setTimeout('moveAction()',speed);
+	}else{//左移动
+		if(com > Space){
+			com -= Space;
+			comdis = Space;
+		}else{
+			comdis = com;
+			com = 0;
+		}
+		adsview.scrollLeft += comdis;
+		setTimeout('moveAction()',speed);
+	}
+}
+
+
+//点击不自动移动
+function stopAutomove(){
+	clearInterval(autoMoveObj);
+}
+
+for(var i = 0; i < 3; i++){
+	var a = document.getElementById('a');
+	console.log(a.childNotes[i]);
+}
